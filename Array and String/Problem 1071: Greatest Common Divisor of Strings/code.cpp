636d2a21c4808774afa61aaeadbb91d197466a3b
Radhika Bhasin <radhikabhasin2408@gmail.com>
2026-01-06 03:35:29 +0530

    Create code.cpp

---
+++ Array and String/Problem 1071: Greatest Common Divisor of Strings/code.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+int getgcd(int n, int m){
+    if(m==0) return n;
+    return getgcd(m,n-m);
+}
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2!=str2+str1) return "";
+        int n1=str1.length();
+        int n2=str2.length();
+
+        int gcd=getgcd(n1,n2);
+        return str1.substr(0,gcd);
+    }
+};
