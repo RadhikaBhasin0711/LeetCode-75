e2d0064b24c2979d0cec69b0526ee182f5d7fe61
Radhika Bhasin <radhikabhasin2408@gmail.com>
2026-01-06 19:27:15 +0530

    Update code.cpp

--- Array and String/Problem 1071: Greatest Common Divisor of Strings/code.cpp
+++ Array and String/Problem 1071: Greatest Common Divisor of Strings/code.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 int getgcd(int n, int m){
     if(m==0) return n;
-    return getgcd(m,n-m);
+    return getgcd(m,n%m);
 }
     string gcdOfStrings(string str1, string str2) {
         if(str1+str2!=str2+str1) return "";
