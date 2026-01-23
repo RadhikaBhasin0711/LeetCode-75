e46c87efe5cc66f665bb4165e459d2e8854b2e7f
Radhika Bhasin <radhikabhasin2408@gmail.com>
2026-01-24 00:22:42 +0530

    Create code.cpp

---
+++ Intervals/Non-overlapping Intervals/code.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        if (n <= 1) return 0;
+        
+        sort(intervals.begin(), intervals.end());
+        
+        int count = 0;                   
+        int prevEnd = intervals[0][1];    
+        
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] < prevEnd) {
+                count++; 
+                prevEnd = min(prevEnd, intervals[i][1]); 
+            } else {
+                prevEnd = intervals[i][1];
+            }
+        }
+        
+        return count;
+    }
+};
