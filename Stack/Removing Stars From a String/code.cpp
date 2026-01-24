19a3f8cf7aa538b335afdc5a15d9df71b7a5df31
Radhika Bhasin <radhikabhasin2408@gmail.com>
2026-01-24 23:14:16 +0530

    Create code.cpp

---
+++ Stack/Removing Stars From a String/code.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string ans="";
+        stack<char> st;
+        for(auto ch:s){
+            if(ch=='*'){
+                if(!st.empty()) st.pop();
+            }
+            else st.push(ch);
+        }
+        while(!st.empty()){
+            ans+=st.top();;
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+
+    }
+};
